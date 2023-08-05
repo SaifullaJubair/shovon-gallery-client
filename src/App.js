32780b97cfaecb7fc193547ba8f5d98f03b8909c
SaifullaJubair <saifullajubair6@gmail.com>
2023-08-06 00:36:17 +0600

    aos and scroll top setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { Button } from "flowbite-react";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes/Routes";
+import { Toaster } from "react-hot-toast";
+import ScrollToTop from "react-scroll-up";
+import { BsArrowUpCircleFill } from "react-icons/bs";
+import { useEffect } from "react";
+import Aos from "aos";
 
 function App() {
+  useEffect(() => {
+    Aos.init();
+    Aos.refresh();
+  }, []);
+
   return (
-    <div className="App">
-      <RouterProvider router={router}>
-        <h1 className="">hi</h1>
-        <Button>Click </Button>
-      </RouterProvider>
+    <div className="max-w-[1440px] mx-auto">
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
+      <ScrollToTop showUnder={160}>
+        <span className="text-4xl text-blue-600 hover:text-sky-600">
+          <BsArrowUpCircleFill className="animate-bounce"></BsArrowUpCircleFill>
+        </span>
+      </ScrollToTop>
     </div>
   );
 }

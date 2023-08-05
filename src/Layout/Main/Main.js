683a49a7d33b277d78b0745c46e79d73b9f0b946
SaifullaJubair <saifullajubair6@gmail.com>
2023-08-05 21:25:59 +0600

    Routing setup

---
+++ src/Layout/Main/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import TopNavbar from "../../Shared/TopNavbar/TopNavbar";
+import SecondNavbar from "../../Shared/SecondNavbar/SecondNavbar";
+import { Footer } from "flowbite-react";
+
+const Main = () => {
+  return (
+    <div>
+      <TopNavbar></TopNavbar>
+      <SecondNavbar></SecondNavbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Shared/SecondNavbar/SecondNavbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SecondNavbar = () => {
+  return <div></div>;
+};
+
+export default SecondNavbar;

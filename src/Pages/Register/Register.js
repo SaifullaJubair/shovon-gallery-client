8416eb233a0062168358e20fa6dcf3dd83e78624
SaifullaJubair <saifullajubair6@gmail.com>
2023-09-28 00:08:46 +0600

    register api correction

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -65,7 +65,7 @@ const Register = () => {
       img: photoURL,
     };
     console.log(user);
-    fetch("http://localhost:5000/users/adduser", {
+    fetch("http://localhost:5000/adduser", {
       method: "POST",
       headers: {
         "content-type": "application/json",
@@ -142,7 +142,7 @@ const Register = () => {
               .then((result) => {
                 // const users =  { name, email, password, createdAt: new Date().toISOString(), photoURL: data?.data?.display_url };
                 // console.log(result)
-                fetch("http://localhost:5000/users/adduser", {
+                fetch("http://localhost:5000/adduser", {
                   method: "POST",
                   headers: {
                     "content-type": "application/json",

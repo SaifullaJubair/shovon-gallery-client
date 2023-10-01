49df36e1ccbda0d56b5219acff6176ce4034fba1
SaifullaJubair <saifullajubair6@gmail.com>
2023-10-01 14:24:38 +0600

    review components

--- src/Pages/Home/CustomerReview/CustomerReview.js
+++ src/Pages/Home/CustomerReview/CustomerReview.js
@@ -12,14 +12,41 @@ import "./CustomerReview.css";
 import { EffectCards } from "swiper/modules";
 import ReviewSlide from "./ReviewSlide";
 const CustomerReview = () => {
+  const slideContent = [
+    {
+      img: "https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
+      text: "So happy with my hair and make up and the hair lasted until the next day photos also. Miss you and thank you again so much for everything.",
+      client: "Jacqui ",
+      corporation: "Hong Kong, China",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
+      text: "Thank you again for your help in making me look glamorous and glad you liked the gift! All the best to you",
+      client: "Jessica",
+      corporation: "Sydney, Australia",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
+      text: "Thank you so much for everything. Not only are you excellent at your job but I really enjoyed meeting you.",
+      client: "Katie",
+      corporation: "Austin, USA",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
+      text: "Thank you so much! My mother, sister and I had such a nice day getting ready and we were all thrilled with our hair and makeup!!",
+      client: "Ivana",
+      corporation: "Moscow, Russia",
+    },
+  ];
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 ">
-      <div class=" text-center  md-w-md my-auto">
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+      <div class="   md-w-md my-auto px-2">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl py-4">
           Don't just take our word for it...
         </h2>
 
-        <p class=" text-gray-700">
+        <p class=" text-gray-700 pb-6">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas
           veritatis illo placeat harum porro optio fugit a culpa sunt id!
         </p>
@@ -31,33 +58,16 @@ const CustomerReview = () => {
           modules={[EffectCards]}
           className="mySwiper"
         >
-          <SwiperSlide>
-            <ReviewSlide></ReviewSlide>
-          </SwiperSlide>
-          <SwiperSlide>
-            <ReviewSlide></ReviewSlide>
-          </SwiperSlide>
-          <SwiperSlide>
-            <ReviewSlide></ReviewSlide>
-          </SwiperSlide>
-          <SwiperSlide>
-            <ReviewSlide></ReviewSlide>
-          </SwiperSlide>
-          <SwiperSlide>
-            <ReviewSlide></ReviewSlide>
-          </SwiperSlide>
-          <SwiperSlide>
-            <ReviewSlide></ReviewSlide>
-          </SwiperSlide>
-          <SwiperSlide>
-            <ReviewSlide></ReviewSlide>
-          </SwiperSlide>
-          <SwiperSlide>
-            <ReviewSlide></ReviewSlide>
-          </SwiperSlide>
-          <SwiperSlide>
-            <ReviewSlide></ReviewSlide>
-          </SwiperSlide>
+          {slideContent.map((el, i) => (
+            <SwiperSlide key={i}>
+              <ReviewSlide
+                client={el.client}
+                corporation={el.corporation}
+                img={el.img}
+                text={el.text}
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
--- src/Pages/Home/CustomerReview/ReviewSlide.js
+++ src/Pages/Home/CustomerReview/ReviewSlide.js
@@ -1,22 +1,19 @@
 import { BsStarFill } from "react-icons/bs";
-function ReviewSlide() {
+function ReviewSlide({ img, text, client, corporation }) {
   return (
     <div className="flex flex-col justify-between">
       <div className="flex gap-2 items-center">
         <img
           className="w-16 h-16 mx-2 rounded-full object-cover object-center shadow-md shadow-secondary/30"
-          src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+          src={img}
           alt="avatar"
         />
         <div className="flex-col flex gap-1">
-          <p className="font-semibold text-lg ">Jacqui</p>
-          <span className="text-[16px]">Hong Kong, China</span>
+          <p className="font-semibold text-lg ">{client}</p>
+          <span className="text-[16px]">{corporation}</span>
         </div>
       </div>
-      <p className="my-4 text-sm px-2 min-h-[96px]">
-        So happy with my hair and make up and the hair lasted until the next day
-        photos also.
-      </p>
+      <p className="my-4 text-sm px-2 min-h-[96px]">{text}</p>
       <div className="flex items-center ml-2 gap-1">
         {Array(5)
           .fill(0)

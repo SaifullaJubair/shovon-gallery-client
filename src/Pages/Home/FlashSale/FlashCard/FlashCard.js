2805c148f74620fc0a2a02ef1c4c0ac2d18569fb
SaifullaJubair <saifullajubair6@gmail.com>
2023-10-31 22:59:11 +0600

    flash sale dynamic

--- src/Pages/Home/FlashSale/FlashCard/FlashCard.js
+++ src/Pages/Home/FlashSale/FlashCard/FlashCard.js
@@ -3,9 +3,30 @@ import { useState } from "react";
 import { BsCart2, BsFillBarChartFill } from "react-icons/bs";
 import { FaExchangeAlt, FaEye, FaHeart, FaStar } from "react-icons/fa";
 import FlashCarousel from "../FlashCarousel";
+import { Link } from "react-router-dom";
 
-const FlashCard = () => {
+const FlashCard = ({ product }) => {
   const [isHovering, setIsHovering] = useState(false);
+  const {
+    _id,
+    product_uid,
+    product_name,
+    category,
+    product_heading,
+    box_content,
+    primary_color,
+    primary_img,
+    price,
+    product_status,
+    available_color,
+    user_email,
+    user_image,
+    user_name,
+    product_highlight,
+    details,
+    feature_img1,
+    feature_img2,
+  } = product;
 
   return (
     <div
@@ -14,64 +35,48 @@ const FlashCard = () => {
       onMouseLeave={() => setIsHovering(false)}
     >
       <div className=" max-w-sm bg-white border border-gray-200  ">
-        <div className="ml-2 mt-2">
-          <span className="bg-gray-200 text-gray-800 text-xs font-medium mr-2 px-2.5 py-1  dark:bg-gray-700 dark:text-gray-300">
-            Dark
-          </span>
-        </div>
-
         {isHovering ? (
           <div>
-            <FlashCarousel></FlashCarousel>
+            <FlashCarousel product={product}></FlashCarousel>
           </div>
         ) : (
-          <img
-            className="p-4 h-56 mx-auto w-56 "
-            src="https://www.pngmart.com/files/7/Lift-Chair-Transparent-PNG.png"
-            alt=""
-          />
+          <div className=" h-60">
+            <img className=" w-full h-full" src={primary_img} alt="" />
+          </div>
         )}
         <div className="px-5 pb-5">
-          <h5 className="text-gray-400 text-sm font-semibold pb-1 ">
-            Smart Tv
+          <h5 className="text-gray-400 text-sm font-semibold py-1 ">
+            {category}
           </h5>
-          <h2 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white ">
-            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+          <h2 className="text-lg font-semibold  text-gray-700 dark:text-white ">
+            {product_heading.slice(0, 45) + "..."}
           </h2>
 
-          {isHovering ? (
-            <div className="py-3  mt-2 mb-4  w-[280px]  bg-white ">
-              <div className="flex items-center justify-around">
-                <FaEye></FaEye>
-                <FaHeart></FaHeart>
-                <FaExchangeAlt></FaExchangeAlt>
-              </div>
-            </div>
-          ) : (
-            <div className="flex items-center mt-2.5 mb-5 text-md text-yellow-300">
-              <FaStar></FaStar>
-              <FaStar></FaStar>
-              <FaStar></FaStar>
-              <FaStar></FaStar>
-              <FaStar></FaStar>
-              <span className="bg-blue-100 text-gray-400 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-                5.0
-              </span>
-            </div>
-          )}
-          <div className="flex items-center ">
-            <span className="font-bold text-[#5B9982] text-base">$ 599</span>
-            <span className=" text-xs text-gray-400 font-semibold ml-2">
-              {" "}
-              <strike>$3000.00</strike>
+          <div className="flex items-center mt-2.5 mb-3 text-md text-yellow-300">
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <span className="bg-blue-100 text-gray-400 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+              5.0
             </span>
           </div>
-          <div className="bg-[#eaefeb] text-[#5B9982] hover:bg-[#5B9982] hover:text-[#eaefeb]  rounded-sm text-center mt-4 font-semibold">
-            <section className="flex items-center justify-center gap-2">
-              <button className=" py-2">Add To Cart </button>{" "}
-              <BsCart2></BsCart2>
-            </section>
+
+          <div className="flex items-center ">
+            <p className="font-semibold text-xl text-secondary">
+              {price}
+              <span className="font-bold text-xl">৳</span>
+            </p>
           </div>
+          <Link to={`/singleproduct/${product?._id}`}>
+            <div className="bg-[#eaefeb] text-[#5B9982] hover:bg-[#5B9982] hover:text-[#eaefeb]  rounded-sm text-center mt-4 font-semibold">
+              <section className="flex items-center justify-center gap-2">
+                <button className=" py-2">Vew Details</button>{" "}
+                <BsCart2></BsCart2>
+              </section>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
--- src/Pages/Home/FlashSale/FlashSale.js
+++ src/Pages/Home/FlashSale/FlashSale.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   BsArrowLeft,
   BsArrowLeftShort,
@@ -7,6 +7,17 @@ import {
 import FlashSaleTitleSection from "./FlashTitle";
 import FlashCard from "./FlashCard/FlashCard";
 const FlashSale = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setProducts(data.slice(0, 4));
+      });
+  }, []);
+  console.log(products);
   return (
     <div className=" mx-auto">
       <div className="flex md:flex-col lg:flex-col xl:flex-col 2xl-flex-row flex-col lg:mx-[60px] md:mx-7 mx-6">
@@ -21,10 +32,9 @@ const FlashSale = () => {
           </div>
           {/* flash sale card section  */}
           <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  mt-6 gap-8 ">
-            <FlashCard></FlashCard>
-            <FlashCard></FlashCard>
-            <FlashCard></FlashCard>
-            <FlashCard></FlashCard>
+            {products.map((product) => (
+              <FlashCard key={product?._id} product={product}></FlashCard>
+            ))}
           </div>
         </div>
       </div>

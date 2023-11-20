1f3c92d1395ea2c2a2208caadcf65ffd65e2c4c1
SaifullaJubair <saifullajubair6@gmail.com>
2023-11-21 01:24:05 +0600

    footer deploy

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -21,7 +21,7 @@ const Footer = () => {
   }, []);
   return (
     <div className=" print:hidden">
-      <footer className="py-10 bg-gray-50 text-gray-900">
+      <footer className="py-10 shadow-md text-gray-900">
         <div className="container px-6 mx-auto space-y-6 divide-y divide-gray-400 md:space-y-12 divide-opacity-50">
           <div className="grid grid-cols-12">
             <div className="pb-6 col-span-full md:pb-0 md:col-span-6">
